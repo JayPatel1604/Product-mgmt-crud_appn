5bbe63e08dd52bc08542d0b06e10c1535074b251
JayPatel1604 <104350555+JayPatel1604@users.noreply.github.com>
2022-05-01 13:51:49 +0530

    Add files via upload

---
+++ ProductMgmtCrudAppnApplication.java
@@ -0,0 +1,15 @@
+package com.cts.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMgmtCrudAppnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMgmtCrudAppnApplication.class, args);
+		System.out.println("ProductMgmtCrudAppApplication is running...."+ "");
+
+	}
+
+}
